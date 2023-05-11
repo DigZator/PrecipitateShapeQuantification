75492818cd5366b5a32ff157a61224b902e2788c
DigZator <omsbhise@gmail.com>
2023-05-11 19:44:23 +0530

    Added comments and add option to pick method

--- reprod2D-2.py
+++ reprod2D-2.py
@@ -1,100 +1,52 @@
 from init import *
 from reprod2D_Test import get_aspect_ratio
 
-# datContent = [i.strip().split() for i in open("./Comp1000.dat").readlines()]
-datContent = [i.strip().split() for i in open("./Comp10000.dat").readlines()]
-
-# print(datContent)
-
-img = np.zeros((512,512))
-img2 = np.zeros((512,512))
-
-for line in datContent:
-    if not line:
-        datContent.pop(datContent.index(line))
-    else:
-        line[0] = int(line[0])
-        line[1] = int(line[1])
-        line[2] = float(line[2])
-        img[line[0], line[1]] = line[2]
-        img2[line[0], line[1]] = 1 - line[2]
-
-
-# cv2.imshow("Image", img)
-# cv2.waitKey(0)
-
-print(np.max(img))
-img = (img * 255).astype(np.uint8)
-
-# cv2.imshow("Image", img)
-# cv2.waitKey(0)
-
-cimg = img.copy()
-# cimg = (np.where(cimg >= 46, 255, 0)).astype(np.uint8)
-thresh, bin_img = cv2.threshold(cimg, (np.max(img)/2), 255, type = cv2.THRESH_BINARY)
-# invbin = ~bin_img
-
-# cv2.imshow("Image", cimg)
-# cv2.waitKey(0)
-
-# cv2.imshow("Image", bin_img)
-# cv2.waitKey(0)
-
-plt.imshow(bin_img)
-plt.show()
-
-# print(bin_img)
-
 ####### Contours Methods #######
 
-# contours, _ = cv2.findContours(bin_img,
-#                                cv2. RETR_TREE,
-#                                cv2.CHAIN_APPROX_SIMPLE)
-
-# img_lis = []
-# bbox_lis = []
-
-# out = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-# out2 = out.copy()
-
-# out = cv2.drawContours(out, contours, -1, (0, 255, 0), 1)
-
-# for c in contours:
-#     x, y, w, h = cv2.boundingRect(c)
-#     if (cv2.contourArea(c)) > 100:
-#         img_lis.append(bin_img[y:(y+h), x:(x+w)])
-#         bbox_lis.append([x,y,w,h])
-#         cv2.rectangle(out2, (x,y), (x+w,y+h), (255,0,0), 1)
-
-# cv2.imshow("Image", out)
-# cv2.waitKey(0)
-
-# takecon, takebbox, takeimg = [], [], []
-# for i in range(len(img_lis)):
-#     cv2.imshow("Precipitate", img_lis[i])
-#     take = cv2.waitKey(0)
-#     # take = int(input("Consider this precipitate"))
-#     if take == ord('y') or take == ord('Y') or take == ord('1'):
-#         # takecon.append(contours[i])
-#         takebbox.append(bbox_lis[i])
-#         takeimg.append(img_lis[i])
-#     cv2.destroyAllWindows()
+def CNT(img, bin_img):
+    # Finding Contours of the precipitates
+    contours, _ = cv2.findContours(bin_img,
+                                cv2. RETR_TREE,
+                                cv2.CHAIN_APPROX_SIMPLE)
     
-#     Alist, RMlist, RElist = [], [], []
+    # To store the Slice of the precipitate and the coordinates of the bounding box
+    img_lis = []
+    bbox_lis = []
 
-# for preci in takeimg:
-#     A, RM, RE = get_aspect_ratio(preci)
-#     print(f"A = {A:.2f}, RM = {RM:.2f}, RE = {RE:.2f}")
-#     Alist.append(A)
-#     RMlist.append(RM)
-#     RElist.append(RE)
+    # Converting the Grayscale image to BGR to making the bounding boxes
+    out = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    out2 = out.copy()
+
+    # Drawing contours on the image
+    out = cv2.drawContours(out, contours, -1, (0, 255, 0), 1)
+
+    # Filtering out smaller contours
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        if (cv2.contourArea(c)) > 100:
+            img_lis.append(bin_img[y:(y+h), x:(x+w)])
+            bbox_lis.append([x,y,w,h])
+            cv2.rectangle(out2, (x,y), (x+w,y+h), (255,0,0), 1)
+
+    cv2.imshow("Image", out)
+    cv2.waitKey(0)
+
+    # Appending the precipitate slices
+    takecon, takebbox, takeimg = [], [], []
+    for i in range(len(img_lis)):
+        cv2.imshow("Precipitate", img_lis[i])
+        take = cv2.waitKey(0)
+        # take = int(input("Consider this precipitate"))
+        if take == ord('y') or take == ord('Y') or take == ord('1'):
+            # takecon.append(contours[i])
+            takebbox.append(bbox_lis[i])
+            takeimg.append(img_lis[i]/255)
+        cv2.destroyAllWindows()
+    return takeimg
 
 ####### End Method #######
 
 ####### HK Algorithm #######
-
-#Dict to store sizes and connections
-clusterAliases = {}
 
 # Make and return a new key for clusterAliases 
 def createcluster(currvar):
@@ -103,15 +55,18 @@ def createcluster(currvar):
     clusterAliases[currvar] = 1
     return currvar
 
+# Increase the size of the cluster and return the label to allot the current pixel
 def addtocluster(label):
     clusterAliases[findoriginal(label)] += 1
     return label
 
+# Find the primary cluster that is connect to this cluster
 def findoriginal(label):
     while clusterAliases[label] < 0:
         label = -clusterAliases[label]
     return label
 
+# Connect the clusters
 def unifycluster(a, l):
     global clusterAliases
     # Cells of same cluster
@@ -134,36 +89,48 @@ def unifycluster(a, l):
     clusterAliases[l] = -OA
     return a
 
+# HK algorithm core
 def hkalg(img):
     currvar = 2
     m, n = len(img), len(img[0])
+
+    # Search pixel by pixel
     for i, j in it.product(range(m), range(n)):
         # Background Cell
         if img[i][j] == 0:
             continue
-    
+        
+        # Check if the adjacent cells are part of a cluster (considering edge cases)
         U = False if i == 0 else (img[i-1][j] != 0)
         L = False if j == 0 else (img[i][j-1] != 0)
 
+        # If both of the adjacent cells are unoccupied, create a new cluster
         if not (U or L):
             currvar = createcluster(currvar)
             img[i][j] = currvar
+        
+        # If only one of them is occupied, add this pixel to the cluster
         elif (U ^ L):
             img[i][j] = addtocluster(img[i-1][j] if U else img[i][j-1])
+        
+        # If both the cells are part of cluster then unify them if required
         elif (U and L):
             U = img[i-1][j]
             L = img[i][j-1]
             img[i][j] = unifycluster(U, L)
+
     return img
     
     # plt.show()
 
+# To match the  label of each pixel with the label of the cluster
 def renamecluster(mat):
     m, n = len(mat), len(mat[0])
     for i, j in it.product(range(m), range(n)):
         if mat[i][j] != 0:
             mat[i][j] = findoriginal(mat[i][j])
 
+# To connect the precipitates that are on the edge of the image
 def connect_across(mat):
     m, n = len(mat), len(mat[0])
     con_listx = []
@@ -184,6 +151,7 @@ def connect_across(mat):
                 mat[i][j] = con[0]
     return mat, con_listx, con_listy
 
+# To make clusters and identify the edge cases.
 def mkcluster(img):
     global currval
     global clusterAliases
@@ -213,7 +181,7 @@ def extract_precipitates(img):
                 clusterdict[clus[i,j]] = [(i, j)]
             else:
                 clusterdict[clus[i,j]].append((i, j))
-    print(clusterdict.keys())
+    # print(clusterdict.keys())
 
     # Converting to numpy array
     for k in clusterdict.keys():
@@ -254,9 +222,61 @@ def extract_precipitates(img):
         preci.append(preci_mat)
     return preci
 
-precipitates = extract_precipitates(bin_img)
 
-for preci in precipitates:
-    preci = preci.astype(int)
-    A, RM, RE = get_aspect_ratio(preci)
-    print(f"A = {A:.2f}, RM = {RM:.2f}, RE = {RE:.2f}, Area = {np.sum(preci):.2f}")
+# datContent = [i.strip().split() for i in open("./Comp1000.dat").readlines()]
+datContent = [i.strip().split() for i in open("./Comp10000.dat").readlines()]
+
+# print(datContent)
+
+img = np.zeros((512,512))
+img2 = np.zeros((512,512))
+
+for line in datContent:
+    if not line:
+        datContent.pop(datContent.index(line))
+    else:
+        line[0] = int(line[0])
+        line[1] = int(line[1])
+        line[2] = float(line[2])
+        img[line[0], line[1]] = line[2]
+        img2[line[0], line[1]] = 1 - line[2]
+
+if __name__ == "__main__":
+    # cv2.imshow("Image", img)
+    # cv2.waitKey(0)
+
+    print(np.max(img))
+    img = (img * 255).astype(np.uint8)
+
+    # cv2.imshow("Image", img)
+    # cv2.waitKey(0)
+
+    cimg = img.copy()
+    # cimg = (np.where(cimg >= 46, 255, 0)).astype(np.uint8)
+    thresh, bin_img = cv2.threshold(cimg, (np.max(img)/2), 255, type = cv2.THRESH_BINARY)
+    # invbin = ~bin_img
+
+    # cv2.imshow("Image", cimg)
+    # cv2.waitKey(0)
+
+    # cv2.imshow("Image", bin_img)
+    # cv2.waitKey(0)
+
+    plt.imshow(bin_img)
+    plt.show()
+
+    # print(bin_img)
+    # method = "CNT"
+    method = "HK"
+    if method == "CNT":
+        precipitates = CNT(img, bin_img)
+
+    elif method == "HK":
+        #Dict to store sizes and connections
+        clusterAliases = {}
+        precipitates = extract_precipitates(bin_img)
+
+    for preci in precipitates:
+        preci = preci.astype(int)
+        A, RM, RE = get_aspect_ratio(preci)
+        print(f"A = {A:.2f}, RM = {RM:.2f}, RE = {RE:.2f}, Area = {np.sum(preci):.2f}")
